a17c385b4483d23bed18af6bb2b733095eb8e053
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2023-09-10 22:13:50 +0200

    Add: match current connected SSID

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to WiFi Auto Connect !
+
+## How to create config file
+First, create a config.ini file in the same directory as the script. Then, open the file and write the following:
+```ini
+[YOUR_SUBCONFIG_NAME]
+SSID = YOUR_SSID
+URL = YOUR_URL
+METHOD = POST
+USERNAME = YOUR_USERNAME
+PASSWORD = YOUR_PASSWORD
+```
+You can add as many sub-configs as you want, just make sure to choose a different name for each section. 
+For example, if you want to add a second subconfig, you can name it [YOUR_SUBCONFIG_NAME2].
--- src/connect.py
+++ src/connect.py
@@ -1,8 +1,15 @@
-import configparser
+from configparser import SectionProxy, ConfigParser
+from enum import Enum
+from subprocess import check_output
+from re import findall, RegexFlag
+
+
+class UrlMethod(Enum):
+    POST = "POST"
 
 
 class Connector:
-    def __init__(self, section: configparser.SectionProxy) -> None:
+    def __init__(self, section: SectionProxy) -> None:
         self._section = section
 
     def check_integrity(self) -> bool:
@@ -10,10 +17,26 @@ class Connector:
         Check if the section has all the required fields
         :return: bool
         """
-        integrity = (hasattr(self._section, 'url')
-                     and hasattr(self._section, 'username')
-                     and hasattr(self._section, 'password'))
+        integrity = ('SSID' in self._section
+                     and 'URL' in self._section
+                     and 'METHOD' in self._section
+                     and 'USERNAME' in self._section
+                     and 'PASSWORD' in self._section)
+
+        integrity = integrity and (self._section["SSID"] != '')
+        integrity = integrity and (self._section["URL"] != '')
+        integrity = integrity and (self._section["METHOD"] != '' and self._section["METHOD"] in UrlMethod.__members__)
         return integrity
+
+    def does_match_current_wifi(self) -> bool:
+        """
+        Check if the current connected Wi-Fi is the same as this one
+        :return: bool
+        """
+        netsh_interfaces = check_output('netsh wlan show interfaces').decode("cp850")
+        ssid_match = findall(r'^\s*SSID\s*.:\s([^\n\r]+)', netsh_interfaces, RegexFlag.MULTILINE | RegexFlag.DOTALL)
+        ssid = ssid_match[0] if len(ssid_match) > 0 else None
+        return ssid is not None and ssid == self._section["SSID"]
 
 
 def main() -> None:
@@ -23,14 +46,20 @@ def main() -> None:
     """
 
     # Read config file
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    config = ConfigParser()
+    if not config.read('config.ini'):
+        print("Config file not found! Please create a config.ini file.")
+        exit(1)
 
     # Get all sections
     sections = config.sections()
     for section in sections:
         connector = Connector(config[section])
-        print(connector.check_integrity())
+        if connector.check_integrity():
+            if connector.does_match_current_wifi():
+                print(f"Section {section} matches current Wi-Fi")
+        else:
+            print(f"Section {section} is invalid")
 
 
 if __name__ == "__main__":
